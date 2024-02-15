83a852e29fbcd8f501781a2f152e3d6ba7cdb8c3
ShubhamMore04 <148211719+ShubhamMore04@users.noreply.github.com>
2024-02-15 19:43:02 +0530

    Delete EMART-1 directory

--- EMART-1/src/main/java/com/example/repositories/ConfigMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.ConfigMaster;
-
-@Repository
-public interface ConfigMasterRepository extends JpaRepository<ConfigMaster, Long> {
-}
--- EMART-1/src/main/java/com/example/services/InvoiceService.java
+++
@@ -1,9 +0,0 @@
-package com.example.services;
-
-import com.example.entities.Invoice;
-
-public interface InvoiceService {
-	Invoice saveInvoice(Invoice obj);
-	Invoice getInvoiceById(int id);
-	Invoice getInvoiceByCustomerId(int id);
-}
